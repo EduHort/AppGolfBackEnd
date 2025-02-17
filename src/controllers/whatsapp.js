8d22a73151312c51a17106afbdeacb9e3f659e7a
EduHort <86855087+EduHort@users.noreply.github.com>
2025-02-17 11:29:12 -0300

    Usando browser

--- src/controllers/whatsapp.js
+++ src/controllers/whatsapp.js
@@ -1,25 +1,31 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const puppeteer = require('puppeteer');
 
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-      headless: true,
-      executablePath: require('puppeteer').executablePath(),
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    },
-  });
-  
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
 
-client.on('qr', (qr) => {
-    console.log('Escaneie o QR Code para conectar no WhatsApp:');
-    qrcode.generate(qr, { small: true });
-});
+    const client = new Client({
+        authStrategy: new LocalAuth(),
+        puppeteer: {
+            browserWSEndpoint: (await browser.wsEndpoint()), // Conecta ao navegador Puppeteer
+        },
+    });
 
-client.on('ready', () => {
-    console.log('WhatsApp Web conectado!');
-});
+    // Exibe o QR Code no terminal
+    client.on('qr', (qr) => {
+        console.log('Escaneie o QR Code para conectar no WhatsApp:');
+        qrcode.generate(qr, { small: true });
+    });
 
-client.initialize();
+    // Confirmação de conexão
+    client.on('ready', () => {
+        console.log('WhatsApp Web conectado!');
+    });
 
-module.exports = client;
+    // Inicia o cliente
+    client.initialize();
+})();
