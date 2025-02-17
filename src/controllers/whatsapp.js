f420e64933c26e5b40b31487940fc6e6ddf9b775
EduHort <86855087+EduHort@users.noreply.github.com>
2025-02-17 12:37:13 -0300

    headless false

--- src/controllers/whatsapp.js
+++ src/controllers/whatsapp.js
@@ -4,7 +4,7 @@ const qrcode = require('qrcode-terminal');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        headless: true,
+        headless: false,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
         ignoreDefaultArgs: ['--disable-extensions'],
     },
