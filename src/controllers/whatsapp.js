c4803077c9447594d305e29c93d215e947ba08f0
EduHort <86855087+EduHort@users.noreply.github.com>
2025-02-20 15:03:00 -0300

    usando google-chrome

--- src/controllers/whatsapp.js
+++ src/controllers/whatsapp.js
@@ -6,7 +6,7 @@ const client = new Client({
     puppeteer: {
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        executablePath: '/usr/bin/chromium-browser',
+        executablePath: '/usr/bin/google-chrome',
     },
 });
 
